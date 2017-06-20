d14e262f6261e09bba94647e5c52c6fa8e79abcf
MatthewPlemmons <matthewleeplemmons@gmail.com>
2017-06-19 22:42:12 -0500

    add flask files

---
+++ flask/app.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+	return render_template('index.html')
+
+if __name__ == "__main__":
+	# Setting debug to True enables debug output. This line should be
+	# removed before deploying a production app.
+	app.debug = True
+	app.run(host="0.0.0.0", port=5000)
