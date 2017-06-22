c11f5f835186dad148590f087a91a34069763ec8
Matthew Plemmons <matthewleeplemmons@gmail.com>
2017-06-22 13:21:59 -0500

    bug fix when pushing data to users array

--- flask/static/scripts/script.js
+++ flask/static/scripts/script.js
@@ -6,7 +6,10 @@ function listItems () {
   $.getJSON('https://yz7z1tqx0e.execute-api.us-east-1.amazonaws.com/dev/api', (data) => {
     let users = [];
     $(data).each(function (i) {
-      users.push("<tr><td>" + data[i].name "</td>" +
+      console.log(data);
+      console.log(data[i]);
+      console.log(data[i].name);
+      users.push("<tr><td>" + data[i].name + "</td>" +
 		 "<td>" + data[i].country + "</td>" +
 		 "<td>" + data[i].created_at + "</td></tr>");
     });
