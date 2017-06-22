dd57ea48136500d02b0822cf305e3ecdbfb71b0f
Matthew Plemmons <matthewleeplemmons@gmail.com>
2017-06-22 13:23:44 -0500

    remove console.log calls

--- flask/static/scripts/script.js
+++ flask/static/scripts/script.js
@@ -6,9 +6,6 @@ function listItems () {
   $.getJSON('https://yz7z1tqx0e.execute-api.us-east-1.amazonaws.com/dev/api', (data) => {
     let users = [];
     $(data).each(function (i) {
-      console.log(data);
-      console.log(data[i]);
-      console.log(data[i].name);
       users.push("<tr><td>" + data[i].name + "</td>" +
 		 "<td>" + data[i].country + "</td>" +
 		 "<td>" + data[i].created_at + "</td></tr>");
